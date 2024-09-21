a8a9fbe843a73f61811c608518194e0eae5f79d9
Jayant Khandelwal <khandelwaljayant1634@gmail.com>
2024-09-21 18:33:37 +0530

    done

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = require('./app');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7860;
 
 // Error handling middleware
 app.use((err, req, res, next) => {
